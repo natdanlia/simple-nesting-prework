e818ba546f57a8a2f22ffe137459797ded39f154
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-08-19 04:22:14 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,13 +15,12 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +36,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     	programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join()
 end
 
 def adding_matz
@@ -79,7 +81,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = { :known_for => "Ruby" }
+#programmer_hash[:yukihiro_matsumoto] = { :languages => ["LISP", "C"] }
+#programmer_hash[:yukihiro_matsumoto]
     
 end
 
